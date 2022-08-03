dee7dd60667b87b8f308691b7f154fd413c08c7f
Jarod GOURLIN <jgourlin@e2r3p10.clusters.42paris.fr>
2022-08-03 18:28:59 +0200

    0.0.3: ajout lexicographical_compare.hpp

---
+++ class/lexicographical_compare.hpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexicographical_compare.hpp                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgourlin <jgourlin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 18:27:41 by jgourlin          #+#    #+#             */
+/*   Updated: 2022/08/03 18:27:47 by jgourlin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXICOGRAPHICAL_COMPARE
+# define LEXICOGRAPHICAL_COMPARE
+
+
+#endif
